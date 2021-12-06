2a9466d2af696ccf280e57b4319086f5f347cabe
María Fernanda Martínez Labra <85769685+mf-mlz@users.noreply.github.com>
2021-12-06 17:08:00 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("respuesta").innerHTML = this.responseText;
+    }
+};
+xmlhttp.open("GET", "api-ip.php", true);
+xmlhttp.send();
